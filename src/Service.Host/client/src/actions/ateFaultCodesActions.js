c26ac59c04083cb3871188f56752e1899a7ab26d
richard phillips <richard.phillips@linn.co.uk>
2019-07-17 17:11:50 +0100

    add new item type

---
+++ src/Service.Host/client/src/actions/ateFaultCodesActions.js
@@ -0,0 +1,11 @@
+﻿import { FetchApiActions } from '@linn-it/linn-form-components-library';
+import { ateFaultCodesActionTypes as actionTypes } from './index';
+import * as itemTypes from '../itemTypes';
+import config from '../config';
+
+export default new FetchApiActions(
+    itemTypes.ateFaultCodes.actionType,
+    itemTypes.ateFaultCodes.uri,
+    actionTypes,
+    config.appRoot
+);
---
+++ src/Service.Host/client/src/reducers/ateFaultCode.js
@@ -0,0 +1,11 @@
+﻿import { itemStoreFactory } from '@linn-it/linn-form-components-library';
+import { ateFaultCodeActionTypes as actionTypes } from '../actions';
+import * as itemTypes from '../itemTypes';
+
+const defaultState = {
+    loading: false,
+    item: null,
+    editStatus: 'view'
+};
+
+export default itemStoreFactory(itemTypes.ateFaultCode.actionType, actionTypes, defaultState);
