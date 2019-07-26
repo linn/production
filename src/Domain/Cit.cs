9c2b700a6fa96e63d73557d0045f2256f3a1b678
Andrew Fraser <andrewfraser73@hotmail.com>
2019-07-26 12:03:43 +0100

    domain

---
+++ src/Domain/Cit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linn.Production.Domain
+{
+    public class Cit
+    {
+        public string Code { get; set;}
+
+        public string Name { get; set; }
+    }
+
+    public class ProductionMeasures
+    {
+        public Cit Cit { get; set; }
+
+        public int? Ones { get; set; }
+
+        public int? Twos { get; set; }
+
+        public int? Threes { get; set; }
+
+        public int? Fours { get; set; }
+
+        public int? Fives { get; set; }
+
+        public double? BuiltThisWeekValue { get; set; }
+
+        public double? BackOrderValue { get; set; }
+
+        public double? StockValue { get; set; }
+
+        public double? OvertStockValue { get; set; }
+
+        public int? NumberOfBackOrders { get; set; }
+
+        public DateTime? OldestBackOrder { get; set; }
+    }
+}
