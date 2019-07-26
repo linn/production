af24fef7ec7935de68f7329893bd237883dd2e58
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-07-26 11:03:52 +0100

    add to serviceDbContext

--- src/Persistence.LinnApps/ServiceDbContext.cs
+++ src/Persistence.LinnApps/ServiceDbContext.cs
@@ -1,4 +1,6 @@
-﻿namespace Linn.Production.Persistence.LinnApps
+﻿using Linn.Production.Domain.LinnApps.SerialNumberReissue;
+
+namespace Linn.Production.Persistence.LinnApps
 {
     using Linn.Common.Configuration;
     using Linn.Production.Domain.LinnApps.ATE;
@@ -13,19 +15,12 @@
 
         public DbSet<AteFaultCode> AteFaultCodes { get; set; }
 
+        public DbSet<SerialNumberReissue> SerialNumberReissues { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             this.BuildAte(builder);
             base.OnModelCreating(builder);
-        }
-
-        protected void BuildAte(ModelBuilder builder)
-        {
-            builder.Entity<AteFaultCode>().ToTable("ATE_TEST_FAULT_CODES");
-            builder.Entity<AteFaultCode>().HasKey(t => t.FaultCode);
-            builder.Entity<AteFaultCode>().Property(t => t.FaultCode).HasColumnName("FAULT_CODE");
-            builder.Entity<AteFaultCode>().Property(t => t.Description).HasColumnName("DESCRIPTION");
-            builder.Entity<AteFaultCode>().Property(t => t.DateInvalid).HasColumnName("DATE_INVALID");
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -42,6 +37,29 @@
             optionsBuilder.UseLoggerFactory(MyLoggerFactory);
             optionsBuilder.EnableSensitiveDataLogging(true);
             base.OnConfiguring(optionsBuilder);
+        }
+
+        private void BuildAte(ModelBuilder builder)
+        {
+            builder.Entity<AteFaultCode>().ToTable("ATE_TEST_FAULT_CODES");
+            builder.Entity<AteFaultCode>().HasKey(t => t.FaultCode);
+            builder.Entity<AteFaultCode>().Property(t => t.FaultCode).HasColumnName("FAULT_CODE");
+            builder.Entity<AteFaultCode>().Property(t => t.Description).HasColumnName("DESCRIPTION");
+            builder.Entity<AteFaultCode>().Property(t => t.DateInvalid).HasColumnName("DATE_INVALID");
+        }
+
+        private void BuildSerialNumberReissues(ModelBuilder builder)
+        {
+            builder.Entity<SerialNumberReissue>().ToTable("SERNOS_RENUM");
+            builder.Entity<SerialNumberReissue>().HasKey(s => s.Id);
+            builder.Entity<SerialNumberReissue>().Property(s => s.Id).HasColumnName("SNRENUM_ID");
+            builder.Entity<SerialNumberReissue>().Property(s => s.SernosGroup).HasColumnName("SERNOS_GROUP").HasMaxLength(10); ;
+            builder.Entity<SerialNumberReissue>().Property(s => s.SerialNumber).HasColumnName("SERNOS_NUMBER");
+            builder.Entity<SerialNumberReissue>().Property(s => s.NewSerialNumber).HasColumnName("NEW_SERNOS_NUMBER");
+            builder.Entity<SerialNumberReissue>().Property(s => s.Comments).HasColumnName("COMMENTS").HasMaxLength(200);
+            builder.Entity<SerialNumberReissue>().Property(s => s.CreatedBy).HasColumnName("CREATED_BY");
+            builder.Entity<SerialNumberReissue>().Property(s => s.ArticleNumber).HasColumnName("ARTICLE_NUMBER").HasMaxLength(14);
+            builder.Entity<SerialNumberReissue>().Property(s => s.NewArticleNumber).HasColumnName("NEW_ARTICLE_NUMBER").HasMaxLength(14);
         }
     }
 }
