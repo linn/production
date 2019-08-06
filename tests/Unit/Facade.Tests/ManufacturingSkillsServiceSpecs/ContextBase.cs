70c5e61831201a7ca779659370c5fe33fddf5424
iainc <iain.crawford@linn.co.uk>
2019-08-06 16:11:53 +0100

    added tests for facade service (for manufacturing skills)

---
+++ tests/Unit/Facade.Tests/ManufacturingSkillsServiceSpecs/ContextBase.cs
@@ -0,0 +1,28 @@
+﻿namespace Linn.Production.Facade.Tests.ManufacturingSkillsServiceSpecs
+{
+    using Linn.Common.Persistence;
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Domain.LinnApps.ATE;
+    using Linn.Production.Facade.Services;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public abstract class ContextBase
+    {
+        protected ManufacturingSkillService Sut { get; set; }
+
+        protected IRepository<ManufacturingSkill, string> ManufacturingSkillRepository { get; private set; }
+
+        protected ITransactionManager TransactionManager { get; private set; }
+
+        [SetUp]
+        public void SetUpContext()
+        {
+            this.ManufacturingSkillRepository = Substitute.For<IRepository<ManufacturingSkill, string>>();
+            this.TransactionManager = Substitute.For<ITransactionManager>();
+            this.Sut = new ManufacturingSkillService(this.ManufacturingSkillRepository, this.TransactionManager);
+        }
+    }
+}
---
+++ tests/Unit/Facade.Tests/ManufacturingSkillsServiceSpecs/WhenAddingManufacturingSkill.cs
@@ -0,0 +1,45 @@
+﻿namespace Linn.Production.Facade.Tests.ManufacturingSkillsServiceSpecs
+{
+    using FluentAssertions;
+    using Linn.Common.Facade;
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Resources;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    public class WhenAddingManufacturingSkill : ContextBase
+    {
+        private ManufacturingSkillResource resource;
+
+        private IResult<ManufacturingSkill> result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.resource = new ManufacturingSkillResource
+            {
+                SkillCode = "skill",
+                Description = "Desc",
+                HourlyRate = 11
+            };
+
+            this.result = this.Sut.Add(this.resource);
+        }
+
+        [Test]
+        public void ShouldAddManufacturingSkill()
+        {
+            this.ManufacturingSkillRepository.Received().Add(Arg.Any<ManufacturingSkill>());
+        }
+
+        [Test]
+        public void ShouldReturnCreated()
+        {
+            this.result.Should().BeOfType<CreatedResult<ManufacturingSkill>>();
+            var dataResult = ((CreatedResult<ManufacturingSkill>)this.result).Data;
+            dataResult.SkillCode.Should().Be("skill");
+            dataResult.Description.Should().Be("Desc");
+            dataResult.HourlyRate.Should().Be(11);
+        }
+    }
+}
