908a17443fc6e96816c3050f0ffb3776a3174497
iainc <iain.crawford@linn.co.uk>
2019-08-09 16:50:24 +0100

    more tests

--- src/Service.Host/client/src/components/__tests__/InputField.DecimalsTests..js
+++ src/Service.Host/client/src/components/__tests__/InputField.DecimalsTests..js
@@ -14,7 +14,7 @@ describe('When Editing', () => {
             <InputField
                 label="Cost"
                 type="number"
-                decimalPlaces={2}
+                decimalPlaces={5}
                 maxLength={14}
                 fullWidth
                 helperText="This field is required"
@@ -32,6 +32,108 @@ describe('When Editing', () => {
             target: { value: '1234.123451' }
         });
 
+        expect(changeMock).toHaveBeenCalledWith('cost', '1234.12345');
+    });
+
+    test('should change input to 2 decimal places from 3', () => {
+        const { getAllByDisplayValue, getByDisplayValue } = render(
+            <InputField
+                label="Cost"
+                type="number"
+                decimalPlaces={2}
+                maxLength={14}
+                fullWidth
+                helperText="This field is required"
+                required
+                propertyName="cost"
+                editStatus="edit"
+                value={0}
+                onChange={changeMock}
+            />
+        );
+
+        const input = getByDisplayValue('0');
+
+        fireEvent.change(input, {
+            target: { value: '1234.123' }
+        });
+
         expect(changeMock).toHaveBeenCalledWith('cost', '1234.12');
+    });
+
+    test('should change input to 2 decimal places from 6', () => {
+        const { getAllByDisplayValue, getByDisplayValue } = render(
+            <InputField
+                label="Cost"
+                type="number"
+                decimalPlaces={2}
+                maxLength={14}
+                fullWidth
+                helperText="This field is required"
+                required
+                propertyName="cost"
+                editStatus="edit"
+                value={0}
+                onChange={changeMock}
+            />
+        );
+
+        const input = getByDisplayValue('0');
+
+        fireEvent.change(input, {
+            target: { value: '1234567.123446' }
+        });
+
+        expect(changeMock).toHaveBeenCalledWith('cost', '1234567.12');
+    });
+    test('should remain the same with no decimal places specified', () => {
+        const { getAllByDisplayValue, getByDisplayValue } = render(
+            <InputField
+                label="Cost"
+                type="number"
+                maxLength={14}
+                fullWidth
+                helperText="This field is required"
+                required
+                propertyName="cost"
+                editStatus="edit"
+                value={0}
+                onChange={changeMock}
+            />
+        );
+
+        const input = getByDisplayValue('0');
+
+        fireEvent.change(input, {
+            target: { value: '1234567.123446' }
+        });
+
+        expect(changeMock).toHaveBeenCalledWith('cost', 1234567.123446);
+    });
+
+    test('should should do nothing when number with no decimals entered', () => {
+        const { getAllByDisplayValue, getByDisplayValue } = render(
+            <InputField
+                label="Cost"
+                type="number"
+                decimalPlaces={2}
+                maxLength={14}
+                fullWidth
+                helperText="This field is required"
+                required
+                propertyName="cost"
+                editStatus="edit"
+                value={0}
+                onChange={changeMock}
+            />
+        );
+
+        const input = getByDisplayValue('0');
+
+        fireEvent.change(input, {
+            target: { value: '1234567' }
+        });
+
+        expect(changeMock).toHaveBeenCalledWith('cost', 1234567);
     });
 });
