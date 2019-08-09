41fb11726f3ca1940e10ee09486c22c0afe5c7a8
iainc <iain.crawford@linn.co.uk>
2019-08-09 11:45:25 +0100

    client side component tests

---
+++ src/Service.Host/client/src/components/__tests__/manufacturingResources.tests.js
@@ -0,0 +1,64 @@
+﻿import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { cleanup } from '@testing-library/react';
+import render from '../../test-utils';
+import ManufacturingResources from '../manufacturingResources/ManufacturingResources';
+
+afterEach(cleanup);
+
+const manufacturingResources = [
+    {
+        resourceCode: 'TESTCODE1',
+        description: 'Descripticon',
+        cost: 10
+    },
+    {
+        resourceCode: 'TESTCODE2',
+        description: 'Descrip',
+        cost: 12
+    }
+];
+const defaultProps = {
+    loading: false,
+    errorMessage: 'there was an error',
+    items: manufacturingResources,
+    history: {}
+};
+
+describe('When Loading', () => {
+    it('should display progress bar', () => {
+        const { getAllByRole } = render(<ManufacturingResources {...defaultProps} loading />);
+        expect(getAllByRole('progressbar').length).toBeGreaterThan(0);
+    });
+
+    it('should not display table', () => {
+        const { queryByRole } = render(<ManufacturingResources {...defaultProps} loading />);
+        expect(queryByRole('table')).not.toBeInTheDocument();
+    });
+});
+
+describe('When viewing', () => {
+    it('should not display progress bar', () => {
+        const { queryByRole } = render(<ManufacturingResources {...defaultProps} loading={false} />);
+        expect(queryByRole('progressbar')).toBeNull();
+    });
+
+    test('Should display table', () => {
+        const { queryByRole } = render(<ManufacturingResources {...defaultProps} />);
+        expect(queryByRole('table')).toBeInTheDocument();
+    });
+
+    test('should display the two manufactruing resources', () => {
+        const { getByText } = render(<ManufacturingResources {...defaultProps} />);
+        const firstResource = getByText('TESTCODE1');
+        const secondResource = getByText('TESTCODE2');
+        expect(firstResource).toBeInTheDocument();
+        expect(secondResource).toBeInTheDocument();
+    });
+
+    test('should display create button', () => {
+        const { getByText } = render(<ManufacturingResources {...defaultProps} />);
+        const input = getByText('Create');
+        expect(input).toBeInTheDocument();
+    });
+});
