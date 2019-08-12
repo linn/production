baa351ba8d36fa1893aff697ca1c1682a76e0f9e
iainc <iain.crawford@linn.co.uk>
2019-08-12 16:44:53 +0100

    finally got persistence module right

--- src/IoC/PersistenceModule.cs
+++ src/IoC/PersistenceModule.cs
@@ -1,12 +1,13 @@
 ﻿namespace Linn.Production.IoC
 {
     using Autofac;
+    using Domain.LinnApps;
+    using Domain.LinnApps.ATE;
+    using Domain.LinnApps.Measures;
+    using Domain.LinnApps.SerialNumberReissue;
+    using Domain.LinnApps.ViewModels;
     using Linn.Common.Persistence;
     using Linn.Common.Persistence.EntityFramework;
-    using Linn.Production.Domain.LinnApps;
-    using Linn.Production.Domain.LinnApps.ATE;
-    using Linn.Production.Domain.LinnApps.Measures;
-    using Linn.Production.Domain.LinnApps.ViewModels;
     using Linn.Production.Persistence.LinnApps;
     using Linn.Production.Persistence.LinnApps.Repositories;
     using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,7 @@
             // linnapps repositories
             builder.RegisterType<DepartmentsRepository>().As<IRepository<Department, string>>();
             builder.RegisterType<AteFaultCodeRepository>().As<IRepository<AteFaultCode, string>>();
+            builder.RegisterType<SerialNumberReissueRepository>().As<IRepository<SerialNumberReissue, int>>();
             builder.RegisterType<ProductionMeasuresRepository>().As<IRepository<ProductionMeasures, string>>();
             builder.RegisterType<ManufacturingSkillsRepository>().As<IRepository<ManufacturingSkill, string>>();
             builder.RegisterType<ManufacturingResourceRepository>().As<IRepository<ManufacturingResource, string>>();
