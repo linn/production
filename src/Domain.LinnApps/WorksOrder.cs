559ef23b2c632b055374c41c7731dbe17e2cd6e0
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-08-29 09:35:38 +0100

    update works order domain

--- src/Domain.LinnApps/WorksOrder.cs
+++ src/Domain.LinnApps/WorksOrder.cs
@@ -1,14 +1,37 @@
 ﻿namespace Linn.Production.Domain.LinnApps
 {
+    using System;
     using System.Collections.Generic;
 
     using Linn.Production.Domain.LinnApps.Measures;
 
     public class WorksOrder
     {
+        public int CancelledBy { get; set; }
+
+        public DateTime DateCancelled { get; set; }
+
+        public DateTime DateRaised { get; set; }
+
+        public string Description { get; set; }
+
         public int OrderNumber { get; set; }
 
         public string PartNumber { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int QuantityBuilt { get; set; }
+
+        public int RaisedBy { get; set; }
+
+        public string RaisedByDepartment { get; set; }
+
+        public string ReasonCancelled { get; set; }
+
+        public string Type { get; set; }
+
+        public string WorkStation { get; set; }
 
         public List<AssemblyFail> AssemblyFails { get; set; }
     }
