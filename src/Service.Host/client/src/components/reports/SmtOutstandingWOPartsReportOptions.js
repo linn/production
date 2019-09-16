d2a245e2181867fff384a5e4cce9a03d2dfc1dc6
richard phillips <richard.phillips@linn.co.uk>
2019-09-16 11:33:15 +0100

    add parts list to options

--- src/Service.Host/client/src/components/reports/SmtOutstandingWOPartsReportOptions.js
+++ src/Service.Host/client/src/components/reports/SmtOutstandingWOPartsReportOptions.js
@@ -1,21 +1,48 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { Dropdown, Title } from '@linn-it/linn-form-components-library';
+import { Dropdown, Title, InputField } from '@linn-it/linn-form-components-library';
 import PropTypes from 'prop-types';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowForward from '@material-ui/icons/ArrowForward';
 import Page from '../../containers/Page';
 
 function SmtOutstandingWOPartsReportOptions({ history }) {
     const [smtLine, setSmtLine] = useState('All');
+    const [partField, setPartField] = useState('');
+    const [parts, setParts] = useState([]);
 
-    const handleClick = () =>
+    const handleClick = () => {
+        let search = `?smtLine=${smtLine}`;
+
+        if (parts.length > 0) {
+            search += '&parts=';
+            search += parts.join('&parts=');
+        }
+
         history.push({
             pathname: `/production/reports/smt/outstanding-works-order-parts/report`,
-            search: `?smtLine=${smtLine}`
+            search
         });
+    };
 
     const handleDropdownChange = (_field, newValue) => {
         setSmtLine(newValue);
+    };
+
+    const addPart = newValue => {
+        setParts([...parts, newValue ? newValue.toUpperCase() : '']);
+        setPartField('');
+    };
+
+    const handleFieldChange = (_, newValue) => {
+        setPartField(newValue);
     };
 
     return (
@@ -36,6 +63,47 @@ function SmtOutstandingWOPartsReportOptions({ history }) {
                     />
                 </Grid>
                 <Grid xs={6} />
+                <Grid xs={12}>
+                    <Typography variant="subtitle2">
+                        Components to show on report - Leave blank to show all parts
+                    </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                    <InputField
+                        label="Part"
+                        maxLength={14}
+                        fullWidth
+                        value={partField}
+                        onChange={handleFieldChange}
+                        propertyName="part"
+                    />
+                </Grid>
+                <Grid xs={1}>
+                    <IconButton
+                        style={{ marginTop: 30 }}
+                        aria-label="select"
+                        onClick={() => addPart(partField)}
+                    >
+                        <ArrowForward fontSize="inherit" />
+                    </IconButton>
+                </Grid>
+                <Grid xs={2} />
+                <Grid xs={4}>
+                    <Typography variant="subtitle2">Components selected</Typography>
+                    <List dense>
+                        {parts.map(p => (
+                            <ListItem>
+                                <ListItemText primary={p} />
+                                <ListItemSecondaryAction>
+                                    <IconButton edge="end" aria-label="delete">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Grid>
+                <Grid xs={2} />
                 <Grid item xs={12}>
                     <Button color="primary" variant="contained" onClick={handleClick}>
                         Run Report
