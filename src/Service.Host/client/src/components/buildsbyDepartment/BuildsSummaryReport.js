a11f78117a4c64cc8e123dfd42587a19c8c3b6a4
lewisre <lewisre@PC1152>
2019-10-01 14:08:28 +0100

    show totals

--- src/Service.Host/client/src/components/buildsbyDepartment/BuildsSummaryReport.js
+++ src/Service.Host/client/src/components/buildsbyDepartment/BuildsSummaryReport.js
@@ -9,29 +9,36 @@ function BuildsSummaryReport({ reportData, loading, history, itemError }) {
         history.push('/production/reports/builds-summary-options');
     };
     if (loading) {
-        return <Loading />;
+        return (
+            <Page showRequestErrors>
+                {' '}
+                <Loading />{' '}
+            </Page>
+        );
     }
     return (
         <Page showRequestErrors>
             {itemError ? (
                 <ErrorCard errorMessage={itemError.details?.message} />
             ) : (
-                <Grid container spacing={3} justify="center">
-                    <Grid item xs={12}>
-                        <ReportTable
-                            reportData={reportData}
-                            title={reportData.title}
-                            showTitle
-                            showTotals={false}
-                            placeholderRows={4}
-                            placeholderColumns={4}
-                            showRowTitles={false}
-                        />
+                reportData && (
+                    <Grid container spacing={3} justify="center">
+                        <Grid item xs={12}>
+                            <ReportTable
+                                reportData={reportData}
+                                title={reportData.title}
+                                showTitle
+                                showTotals
+                                placeholderRows={4}
+                                placeholderColumns={4}
+                                showRowTitles={false}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <BackButton backClick={() => handleBackClick(history)} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <BackButton backClick={() => handleBackClick(history)} />
-                    </Grid>
-                </Grid>
+                )
             )}
         </Page>
     );
