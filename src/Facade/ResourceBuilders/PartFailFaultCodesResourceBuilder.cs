0521f1499df494782ebfb1f3c741ca711c028416
lewisre <lewisre@PC1152>
2019-10-04 10:45:40 +0100

    forgot to save file

--- src/Facade/ResourceBuilders/PartFailFaultCodesResourceBuilder.cs
+++ src/Facade/ResourceBuilders/PartFailFaultCodesResourceBuilder.cs
@@ -1,7 +1,29 @@
 ﻿namespace Linn.Production.Facade.ResourceBuilders
 {
-    public class PartFailFaultCodesResourceBuilder
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Linn.Common.Facade;
+    using Linn.Production.Domain.LinnApps.Measures;
+    using Linn.Production.Resources;
+
+    public class PartFailFaultCodesResourceBuilder : IResourceBuilder<IEnumerable<PartFailFaultCode>>
     {
-        
+        private readonly PartFailFaultCodeResourceBuilder partFailFaultCodeResourceBuilder = new PartFailFaultCodeResourceBuilder();
+
+        public IEnumerable<PartFailFaultCodeResource> Build(IEnumerable<PartFailFaultCode> partFailFaultCodes)
+        {
+            return partFailFaultCodes
+                .OrderBy(b => b.FaultCode)
+                .Select(a => this.partFailFaultCodeResourceBuilder.Build(a));
+        }
+
+        object IResourceBuilder<IEnumerable<PartFailFaultCode>>.Build(IEnumerable<PartFailFaultCode> partFailFaultCodes) => this.Build(partFailFaultCodes);
+
+        public string GetLocation(IEnumerable<PartFailFaultCode> partFailFaultCodes)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
