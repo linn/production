717f5d0f0873f0314a304a7b383984feb1c0b74a
Adam Campbell <Adam.Campbell@linn.co.uk>
2019-10-22 13:49:48 +0100

    remove from domain csproj

--- src/Domain.LinnApps/RemoteServices/IOutstandingWorksOrdersReportDatabaseService.cs
+++ src/Domain.LinnApps/RemoteServices/IOutstandingWorksOrdersReportDatabaseService.cs
@@ -2,8 +2,6 @@
 {
     using System.Data;
 
-    using Linn.Production.Resources;
-
     public interface IOutstandingWorksOrdersReportDatabaseService
     {
         DataTable GetReport(string reportType, string searchParameter);
