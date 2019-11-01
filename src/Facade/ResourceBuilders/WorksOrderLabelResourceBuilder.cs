50e8f040e669a165ebe5deedd6e85e5f56050f85
lewisre <lewisre@PC1152>
2019-11-01 14:10:52 +0000

    resource and facade

---
+++ src/Facade/ResourceBuilders/WorksOrderLabelResourceBuilder.cs
@@ -0,0 +1,34 @@
+﻿namespace Linn.Production.Facade.ResourceBuilders
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Resources;
+    using Linn.Production.Domain.LinnApps.WorksOrders;
+    using Linn.Production.Resources;
+
+    public class WorksOrderLabelResourceBuilder : IResourceBuilder<WorksOrderLabel>
+    {
+        public WorksOrderLabelResource Build(WorksOrderLabel model)
+        {
+            return new WorksOrderLabelResource
+                       {
+                           PartNumber = model.PartNumber, Sequence = model.Sequence, LabelText = model.LabelText,
+                           Links = this.BuildLinks(model).ToArray()
+                       };
+        }
+
+        public string GetLocation(WorksOrderLabel model)
+        {
+            return $"/production/works-orders/labels/{model.PartNumber}-{model.Sequence}";
+        }
+
+        object IResourceBuilder<WorksOrderLabel>.Build(WorksOrderLabel worksOrder) => this.Build(worksOrder);
+
+        private IEnumerable<LinkResource> BuildLinks(WorksOrderLabel worksOrder)
+        {
+            yield return new LinkResource { Rel = "self", Href = this.GetLocation(worksOrder) };
+        }
+    }
+}
---
+++ src/Facade/Services/WorksOrderLabelsService.cs
@@ -0,0 +1,33 @@
+﻿namespace Linn.Production.Facade.Services
+{
+    using System;
+    using System.Linq.Expressions;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Persistence;
+    using Linn.Production.Domain.LinnApps.WorksOrders;
+    using Linn.Production.Resources;
+
+    public class WorksOrderLabelsService : FacadeService<WorksOrderLabel, WorksOrderLabelKey, WorksOrderLabelResource, WorksOrderLabelResource>
+    {
+        public WorksOrderLabelsService(IRepository<WorksOrderLabel, WorksOrderLabelKey> repository, ITransactionManager transactionManager)
+            : base(repository, transactionManager)
+        {
+        }
+
+        protected override WorksOrderLabel CreateFromResource(WorksOrderLabelResource resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateFromResource(WorksOrderLabel entity, WorksOrderLabelResource updateResource)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Expression<Func<WorksOrderLabel, bool>> SearchExpression(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
