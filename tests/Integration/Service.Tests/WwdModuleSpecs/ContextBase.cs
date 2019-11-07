4f570850b6885a08b27b476c72c07b9be39033b4
Andrew Fraser <andrewfraser73@hotmail.com>
2019-11-07 15:45:04 +0000

    integration test

---
+++ tests/Integration/Service.Tests/WwdModuleSpecs/ContextBase.cs
@@ -0,0 +1,37 @@
+﻿namespace Linn.Production.Service.Tests.WwdModuleSpecs
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using Linn.Common.Facade;
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Domain.LinnApps.Triggers;
+    using Linn.Production.Facade.ResourceBuilders;
+    using Linn.Production.Facade.Services;
+    using Linn.Production.Service.Modules;
+    using Linn.Production.Service.ResponseProcessors;
+    using Nancy.Testing;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    public abstract class ContextBase : NancyContextBase
+    {
+        protected IWwdResultFacadeService WwdResultFacadeService { get; private set; }
+
+        [SetUp]
+        public void EstablishContext()
+        {
+            this.WwdResultFacadeService = Substitute.For<IWwdResultFacadeService>();
+
+            var bootstrapper = new ConfigurableBootstrapper(
+                with =>
+                {
+                    with.Dependency(this.WwdResultFacadeService);
+                    with.Dependency<IResourceBuilder<WwdResult>>(new WwdResultResourceBuilder());
+                    with.Module<WwdModule>();
+                    with.ResponseProcessor<WwdResultReportResponseProcessor>();
+                });
+
+            this.Browser = new Browser(bootstrapper);
+        }
+    }
+}
