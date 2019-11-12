2c554cf08c307d74436997765ceba2c9de9065f3
lewisre <lewisre@PC1152>
2019-11-12 09:56:04 +0000

    forgot to save

--- src/Service.Host/client/src/components/__tests__/ProductionMeasureTests/TabCell.test.js
+++ src/Service.Host/client/src/components/__tests__/ProductionMeasureTests/TabCell.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import TableBody from '@material-ui/core/TableBody';
 import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
 import { cleanup } from '@testing-library/react';
 import render from '../../../test-utils';
 import TabCell from '../../reports/measures/TabCell';
-import TableRow from '@material-ui/core/TableRow';
 
 const wrapInTable = children => (
     <Table>
@@ -31,14 +31,16 @@ describe('<TabCell />', () => {
         });
     });
 
-    // describe('When different index', () => {
-    //     it('should display tabcell contents', () => {
-    //         const { queryByText } = render(
-    //             <TabCell index={2} value={1}>
-    //                 {'test'}
-    //             </TabCell>
-    //         );
-    //         expect(queryByText('test')).toBeNull();
-    //     });
-    // });
+    describe('When different index', () => {
+        it('should display tabcell contents', () => {
+            const { queryByText } = render(
+                wrapInTable(
+                    <TabCell index={2} value={1}>
+                        {'test'}
+                    </TabCell>
+                )
+            );
+            expect(queryByText('test')).toBeNull();
+        });
+    });
 });
