70b7513eee8df39a13e291a0657e291649b4f4a9
iainc <iain.crawford@linn.co.uk>
2019-11-20 12:32:51 +0000

    added stylesheet and implemented it for printing on two reports

--- src/Service.Host/client/src/components/reports/ManufacturingCommitDateReport.js
+++ src/Service.Host/client/src/components/reports/ManufacturingCommitDateReport.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import Typography from '@material-ui/core/Typography';
 import Page from '../../containers/Page';
+import '../printStyles.css';
 
 const ManufacturingCommitDateReport = ({ reportData, loading, options }) => {
     if (loading) {
@@ -13,88 +14,92 @@ const ManufacturingCommitDateReport = ({ reportData, loading, options }) => {
 
     if (reportData) {
         return (
-            <Page>
-                <Grid container spacing={3} justify="center">
-                    <Grid item xs={12}>
-                        <Title
-                            text={`Manufacturing Commit Date Results For ${moment(
-                                options.date
-                            ).format('DD-MMM-YYYY')}`}
-                        />
+            <div className="pageContainer">
+                <Page>
+                    <Grid container spacing={3} justify="center">
+                        <Grid item xs={12}>
+                            <Title
+                                text={`Manufacturing Commit Date Results For ${moment(
+                                    options.date
+                                ).format('DD-MMM-YYYY')}`}
+                            />
+                        </Grid>
+                        {reportData.results.map((result, i) => (
+                            // eslint-disable-next-line
+                            <Fragment key={i}>
+                                <Grid item xs={12}>
+                                    <Typography variant="h5">
+                                        Product Type:{' '}
+                                        {result.productType ? result.productType : 'None'}
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Typography variant="h6">
+                                        No Of Lines: {result.numberOfLines}
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Typography variant="h6">
+                                        Supplied: {result.numberSupplied} (
+                                        {result.percentageSupplied}%)
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <Typography variant="h6">
+                                        Available: {result.numberAvailable} (
+                                        {result.percentageAvailable}%)
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={4} />
+                                <Grid item xs={12}>
+                                    <ReportTable
+                                        reportData={result.results.reportResults[0]}
+                                        title={result.results.reportResults[0].title}
+                                        showTitle={false}
+                                        showTotals={false}
+                                        placeholderRows={4}
+                                        placeholderColumns={7}
+                                        showRowTitles={false}
+                                    />
+                                </Grid>
+                            </Fragment>
+                        ))}
+                        <Grid item xs={12}>
+                            <Typography variant="h5">Totals:</Typography>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Typography variant="h6" gutterBottom>
+                                No Of Lines: {reportData.totals.numberOfLines}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6">
+                                Supplied: {reportData.totals.numberSupplied} (
+                                {reportData.totals.percentageSupplied}%)
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6">
+                                Available: {reportData.totals.numberAvailable} (
+                                {reportData.totals.percentageAvailable}%)
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4} />
+                        <Grid item xs={6}>
+                            <ReportTable
+                                reportData={reportData.incompleteLinesAnalysis.reportResults[0]}
+                                title={reportData.incompleteLinesAnalysis.reportResults[0].title}
+                                showTitle
+                                showTotals
+                                placeholderRows={4}
+                                placeholderColumns={2}
+                                showRowTitles
+                            />
+                        </Grid>
+                        <Grid item xs={6} />
                     </Grid>
-                    {reportData.results.map((result, i) => (
-                        // eslint-disable-next-line
-                        <Fragment key={i}>
-                            <Grid item xs={12}>
-                                <Typography variant="h5">
-                                    Product Type: {result.productType ? result.productType : 'None'}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={2}>
-                                <Typography variant="h6">
-                                    No Of Lines: {result.numberOfLines}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Typography variant="h6">
-                                    Supplied: {result.numberSupplied} ({result.percentageSupplied}%)
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <Typography variant="h6">
-                                    Available: {result.numberAvailable} (
-                                    {result.percentageAvailable}%)
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={4} />
-                            <Grid item xs={12}>
-                                <ReportTable
-                                    reportData={result.results.reportResults[0]}
-                                    title={result.results.reportResults[0].title}
-                                    showTitle={false}
-                                    showTotals={false}
-                                    placeholderRows={4}
-                                    placeholderColumns={7}
-                                    showRowTitles={false}
-                                />
-                            </Grid>
-                        </Fragment>
-                    ))}
-                    <Grid item xs={12}>
-                        <Typography variant="h5">Totals:</Typography>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Typography variant="h6" gutterBottom>
-                            No Of Lines: {reportData.totals.numberOfLines}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Typography variant="h6">
-                            Supplied: {reportData.totals.numberSupplied} (
-                            {reportData.totals.percentageSupplied}%)
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Typography variant="h6">
-                            Available: {reportData.totals.numberAvailable} (
-                            {reportData.totals.percentageAvailable}%)
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={4} />
-                    <Grid item xs={6}>
-                        <ReportTable
-                            reportData={reportData.incompleteLinesAnalysis.reportResults[0]}
-                            title={reportData.incompleteLinesAnalysis.reportResults[0].title}
-                            showTitle
-                            showTotals
-                            placeholderRows={4}
-                            placeholderColumns={2}
-                            showRowTitles
-                        />
-                    </Grid>
-                    <Grid item xs={6} />
-                </Grid>
-            </Page>
+                </Page>
+            </div>
         );
     }
 
--- src/Service.Host/client/src/components/reports/OverdueOrdersReport.js
+++ src/Service.Host/client/src/components/reports/OverdueOrdersReport.js
@@ -4,37 +4,46 @@ import Typography from '@material-ui/core/Typography';
 import { ReportTable, Loading, Title, ErrorCard } from '@linn-it/linn-form-components-library';
 import PropTypes from 'prop-types';
 import Page from '../../containers/Page';
+import '../printStyles.css';
 
 export default function OverdueOrdersReport({ reportData, loading, options, error }) {
     return (
-        <Page width="xl">
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Title text="Outstanding Sales Orders by Days Late" />
-                </Grid>
-                {error && (
+        <div className="pageContainer">
+            <Page width="xl">
+                <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <ErrorCard errorMessage={error} />
+                        <Title text="Outstanding Sales Orders by Days Late" />
                     </Grid>
-                )}
-                <Grid item xs={12}>
-                    {options.daysMethod === 'Working Days' ? (
-                        <Typography variant="subtitle2">Days Late are only working days</Typography>
-                    ) : (
-                        <Typography variant="subtitle2">
-                            Days Late include weekends and holidays
-                        </Typography>
+                    {error && (
+                        <Grid item xs={12}>
+                            <ErrorCard errorMessage={error} />
+                        </Grid>
                     )}
+                    <Grid item xs={12}>
+                        {options.daysMethod === 'Working Days' ? (
+                            <Typography variant="subtitle2">
+                                Days Late are only working days
+                            </Typography>
+                        ) : (
+                            <Typography variant="subtitle2">
+                                Days Late include weekends and holidays
+                            </Typography>
+                        )}
+                    </Grid>
+                    <Grid item xs={12}>
+                        {loading ? (
+                            <Loading />
+                        ) : (
+                            <ReportTable
+                                reportData={reportData}
+                                showTotals={false}
+                                showTitle={false}
+                            />
+                        )}
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    {loading ? (
-                        <Loading />
-                    ) : (
-                        <ReportTable reportData={reportData} showTotals={false} showTitle={false} />
-                    )}
-                </Grid>
-            </Grid>
-        </Page>
+            </Page>
+        </div>
     );
 }
 
