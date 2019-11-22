515e2b0c4743f3b2752e814b78f21150c5652948
iaincrawf <51612225+iaincrawf@users.noreply.github.com>
2019-11-22 10:37:09 +0000

    added styles for printing to fit large table on a4 paper (#179)
    * added stylesheet and implemented it for printing on two reports
    
    * upped components library version
    
    * css moved to assets and container div moved to index js
    
    * css zoom change slightly for widest table
    
    * package lock

--- src/Service.Host/client/src/components/reports/OverdueOrdersReport.js
+++ src/Service.Host/client/src/components/reports/OverdueOrdersReport.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { ReportTable, Loading, Title, ErrorCard } from '@linn-it/linn-form-components-library';
@@ -7,34 +7,42 @@ import Page from '../../containers/Page';
 
 export default function OverdueOrdersReport({ reportData, loading, options, error }) {
     return (
-        <Page width="xl">
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Title text="Outstanding Sales Orders by Days Late" />
-                </Grid>
-                {error && (
+        <Fragment>
+            <Page width="xl">
+                <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <ErrorCard errorMessage={error} />
+                        <Title text="Outstanding Sales Orders by Days Late" />
                     </Grid>
-                )}
-                <Grid item xs={12}>
-                    {options.daysMethod === 'Working Days' ? (
-                        <Typography variant="subtitle2">Days Late are only working days</Typography>
-                    ) : (
-                        <Typography variant="subtitle2">
-                            Days Late include weekends and holidays
-                        </Typography>
+                    {error && (
+                        <Grid item xs={12}>
+                            <ErrorCard errorMessage={error} />
+                        </Grid>
                     )}
+                    <Grid item xs={12}>
+                        {options.daysMethod === 'Working Days' ? (
+                            <Typography variant="subtitle2">
+                                Days Late are only working days
+                            </Typography>
+                        ) : (
+                            <Typography variant="subtitle2">
+                                Days Late include weekends and holidays
+                            </Typography>
+                        )}
+                    </Grid>
+                    <Grid item xs={12}>
+                        {loading ? (
+                            <Loading />
+                        ) : (
+                            <ReportTable
+                                reportData={reportData}
+                                showTotals={false}
+                                showTitle={false}
+                            />
+                        )}
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    {loading ? (
-                        <Loading />
-                    ) : (
-                        <ReportTable reportData={reportData} showTotals={false} showTitle={false} />
-                    )}
-                </Grid>
-            </Grid>
-        </Page>
+            </Page>
+        </Fragment>
     );
 }
 
