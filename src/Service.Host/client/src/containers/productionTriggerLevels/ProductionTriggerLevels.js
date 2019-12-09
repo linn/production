d644004c8c585916ee0d83ca204f7a4e2cfad617
iaincrawf <51612225+iaincrawf@users.noreply.github.com>
2019-12-09 11:04:45 +0000

    Trigger levels search fix (#197)
    * some back end from trigger levels
    
    * builder implemented and tests fixed
    
    * fixes so will build
    
    * removed comment
    
    * changed char? to string for temporary and tests now pass, added tests for when adding, also some front end work
    
    * trigger levels search page implemented and click through fixed
    
    * front end work, work station search implemented on back end
    
    * implementation to pull in work stations and most work except create
    
    * implemented allowedToEdit check on fields in trigger level, added front end tests
    
    * test formatting
    
    * pr fixes and tests fiiiiiinally fixed with no warnings
    
    * added to menu
    
    * fix somehow changed uri in itemTypes, fix search and add cits names to display on search page as well as code
    
    * test fix

--- src/Service.Host/client/src/containers/productionTriggerLevels/ProductionTriggerLevels.js
+++ src/Service.Host/client/src/containers/productionTriggerLevels/ProductionTriggerLevels.js
@@ -1,18 +1,28 @@
 import { connect } from 'react-redux';
-import { getItemError } from '@linn-it/linn-form-components-library';
+import { getItemError, initialiseOnMount } from '@linn-it/linn-form-components-library';
 import ViewManufacturingRoutes from '../../components/productionTriggerLevels/TriggerLevels';
 import productionTriggerLevelsActions from '../../actions/productionTriggerLevelsActions';
 import productionTriggerLevelsSelectors from '../../selectors/productionTriggerLevelsSelectors';
+import citsActions from '../../actions/citsActions';
+import citsSelectors from '../../selectors/citsSelectors';
 import * as itemTypes from '../../itemTypes';
 
 const mapStateToProps = state => ({
     items: productionTriggerLevelsSelectors.getItems(state),
     loading: productionTriggerLevelsSelectors.getLoading(state),
-    itemError: getItemError(state, itemTypes.productionTriggerLevels.item)
+    itemError: getItemError(state, itemTypes.productionTriggerLevels.item),
+    cits: citsSelectors.getItems(state)
 });
 
+const initialise = () => dispatch => {
+    dispatch(citsActions.fetch());
+};
 const mapDispatchToProps = {
-    fetchItems: productionTriggerLevelsActions.fetchByQueryString
+    fetchItems: productionTriggerLevelsActions.fetchByQueryString,
+    initialise
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ViewManufacturingRoutes);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(initialiseOnMount(ViewManufacturingRoutes));
