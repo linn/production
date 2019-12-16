580e02eb413f759e046a332594d7fee03a6e31de
iainc <iain.crawford@linn.co.uk>
2019-12-16 09:40:08 +0000

    new files to populate dropdowns

---
+++ src/Domain.LinnApps/GeneralPurposeLabelTypes.cs
@@ -0,0 +1,24 @@
+﻿namespace Linn.Production.Domain.LinnApps
+{
+    using System.Collections.Generic;
+
+    public class GeneralPurposeLabelTypes
+    {
+
+        public const string LargeWeeText = "Large label (wee text)";
+
+        public const string LargeBigText = "Large label (big text)";
+
+        public const string Small = "Small";
+
+        public const string PCNumbers = "PC Numbers";
+
+        public const string AddressLabel = "Address Label";
+
+        public const string GoodsInLabel = "GoodsInLabel";
+
+        public const string SmallWeeText = "Small (wee text)";
+
+        public const string SmallBoldText = "Small (wee bold text)";
+    }
+}
---
+++ src/Domain.LinnApps/LabelPrinters.cs
@@ -0,0 +1,41 @@
+﻿namespace Linn.Production.Domain.LinnApps
+{
+    using System.Collections.Generic;
+    using System.Reflection.Metadata;
+
+    public class LabelPrinters
+    {
+        public const string GoodsInLarge = "Goods In Labels (Small)";
+
+        public const string GoodsInSmall = "Goods In Labels (Large)";
+
+        public const string ProductionSmall = "Production Label Printer (Small)";
+
+        public const string ProductionLarge = "Production Label Printer (Large)";
+
+        //public const string CastleMilk = "Castlemilk Labels";
+
+        public const string SMT = "SMT Labels";
+
+        public const string Test = "Test Label";
+
+        public const string MatHand = "Mat Hand Labels";
+
+        public const string Lp12Small = "LP12 Small (B133)";
+
+        public const string Lp12Large = "LP12 Large (B133)";
+
+        public const string DsmSmall = "DSM Small (B57)";
+
+        public const string DsmLarge = "DSM Large (BB7)";
+
+        public const string FlexibleSmall = "Flexible Small (B104)";
+
+        public const string FlexibleLarge = "Flexible Large(B104)";
+
+        public const string KikoSmall = "Kiko Small (op B30)";
+
+        public const string KikoLarge = "Kiko Large (op B30)";
+
+    }
+}
