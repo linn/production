9c66285a11c5ed9f6b71a74ee93654457117a5b3
iainc <iain.crawford@linn.co.uk>
2019-12-16 10:05:24 +0000

    added label print model

---
+++ src/Domain.LinnApps/LabelPrint.cs
@@ -0,0 +1,15 @@
+﻿namespace Linn.Production.Domain.LinnApps
+{
+    using System;
+
+    public class LabelPrint
+    {
+        public string Type { get; set; }
+
+        public string Printer { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string LineOne { get; set; }
+    }
+}
