47b497c63c4a663040932a9194db6d1ceba05379
iainc <iain.crawford@linn.co.uk>
2020-01-15 16:15:29 +0000

    oops it was already on this page

--- src/Service.Host/client/src/components/productionTriggerLevels/TriggerLevel.js
+++ src/Service.Host/client/src/components/productionTriggerLevels/TriggerLevel.js
@@ -235,37 +235,28 @@ function TriggerLevel({
                                             helperText="This field cannot be changed - description comes from part number"
                                         />
                                     </Grid>
-                                    <Grid item xs={creating() ? 12 : 6}>
-                                        <Dropdown
-                                            onChange={handleResourceFieldChange}
-                                            items={cits.map(cit => ({
-                                                ...cit,
-                                                id: cit.code,
-                                                displayText: `${cit.code} (${cit.name})`
-                                            }))}
-                                            value={triggerLevel.citCode}
-                                            propertyName="citCode"
-                                            helperText={
-                                                citCodeInvalid() ? 'This field is required' : ''
-                                            }
-                                            required
-                                            fullWidth
-                                            label="CIT Code"
-                                            allowNoValue
-                                            disabled={!allowedToEdit}
-                                        />
-                                    </Grid>
-                                    {!creating() && (
+                                    <Grid item xs={12}>
                                         <Grid item xs={6}>
-                                            <InputField
-                                                value={triggerLevel.routeCode}
-                                                label="Route Code"
+                                            <Dropdown
+                                                onChange={handleResourceFieldChange}
+                                                items={cits.map(cit => ({
+                                                    ...cit,
+                                                    id: cit.code,
+                                                    displayText: `${cit.code} (${cit.name})`
+                                                }))}
+                                                value={triggerLevel.citCode}
+                                                propertyName="citCode"
+                                                helperText={
+                                                    citCodeInvalid() ? 'This field is required' : ''
+                                                }
+                                                required
                                                 fullWidth
-                                                propertyName="routeCode"
-                                                disabled
+                                                label="CIT Code"
+                                                allowNoValue
+                                                disabled={!allowedToEdit}
                                             />
                                         </Grid>
-                                    )}
+                                    </Grid>
 
                                     {!creating() && (
                                         <Grid item xs={6}>
