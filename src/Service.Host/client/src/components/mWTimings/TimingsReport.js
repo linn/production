3e13d2020b5cf78e6522c71f2bd2c20bf6b0d715
iainc <iain.crawford@linn.co.uk>
2020-02-26 14:16:58 +0000

    timings fixed

--- src/Service.Host/client/src/components/mWTimings/TimingsReport.js
+++ src/Service.Host/client/src/components/mWTimings/TimingsReport.js
@@ -10,42 +10,54 @@ import {
     ErrorCard
 } from '@linn-it/linn-form-components-library';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
 import Page from '../../containers/Page';
+
+const useStyles = makeStyles(theme => ({
+    topMargin: {
+        marginTop: theme.spacing(3),
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2)
+    }
+}));
 
 function TimingsReport({ reportData, loading, config, errorMessage, options }) {
     const href = `${config.appRoot}/production/reports/mw-timings/export?startDate=${options.startDate}&endDate=${options.endDate}`;
+    const classes = useStyles();
 
     return (
-        <Page>
-            <Grid container spacing={3} justify="center">
-                {errorMessage && (
-                    <Grid item xs={12}>
-                        <ErrorCard errorMessage={errorMessage} />
+        <>
+            <Page>
+                <Grid container spacing={3} justify="center">
+                    {errorMessage && (
+                        <Grid item xs={12}>
+                            <ErrorCard errorMessage={errorMessage} />
+                        </Grid>
+                    )}
+                    <Grid item xs={8}>
+                        <Title text={reportData?.title?.displayString} />
                     </Grid>
-                )}
-                <Grid item xs={8}>
-                    <Title text={reportData?.title?.displayString} />
+                    <Grid item xs={4}>
+                        <ExportButton href={href} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Link component={RouterLink} to="/production/reports/mw-timings-setup">
+                            Run this report for different dates
+                        </Link>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <ExportButton href={href} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Link component={RouterLink} to="/production/reports/mw-timings-setup">
-                        Run this report for different parameters
-                    </Link>
-                </Grid>
-                <Grid item xs={12}>
-                    {loading ? <Loading /> : ''}
-                    <ReportTable
-                        reportData={reportData}
-                        showTotals
-                        placeholderRows={10}
-                        placeholderColumns={11}
-                        showTitle
-                    />
-                </Grid>
+            </Page>
+            <Grid item xs={12} className={classes.topMargin}>
+                {loading ? <Loading /> : ''}
+                <ReportTable
+                    reportData={reportData}
+                    showTotals
+                    placeholderRows={10}
+                    placeholderColumns={11}
+                    showTitle
+                />
             </Grid>
-        </Page>
+        </>
     );
 }
 
