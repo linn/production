39d19d366b82eb790a3170fc8bb8a42c474ff9ad
lewisre <lewisre@PC1152>
2021-08-20 15:33:01 +0100

    implement missing method

--- src/Persistence.LinnApps/Repositories/AteTestDetailRepository.cs
+++ src/Persistence.LinnApps/Repositories/AteTestDetailRepository.cs
@@ -43,7 +43,8 @@
 
         public AteTestDetail FindBy(Expression<Func<AteTestDetail, bool>> expression)
         {
-            throw new NotImplementedException();
+            return this.serviceDbContext
+                .AteTestDetails.Where(expression).ToList().FirstOrDefault();
         }
 
         public IQueryable<AteTestDetail> FilterBy(Expression<Func<AteTestDetail, bool>> expression)
