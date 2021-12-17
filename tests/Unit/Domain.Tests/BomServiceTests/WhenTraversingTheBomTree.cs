174f05313da4a5d9cf29238e01999d8e8dc35fca
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2021-12-17 12:15:14 +0000

    check nodes added in correct order

--- tests/Unit/Domain.Tests/BomServiceTests/WhenTraversingTheBomTree.cs
+++ tests/Unit/Domain.Tests/BomServiceTests/WhenTraversingTheBomTree.cs
@@ -48,6 +48,8 @@
 
         private Bom n1121;
 
+        private List<Bom> result;
+
         [SetUp]
         public void SetUp()
         {
@@ -63,13 +65,25 @@
                this.n111,
                this.n112,
                this.n1121);
+
+           this.result = this.Sut.GetAllAssembliesOnBom("ROOT").ToList();
         }
 
         [Test]
 
         public void ShouldAddAllNodesToResultListBreadthFirst()
         {
-            this.Sut.GetAllAssembliesOnBom("ROOT").Count().Should().Be(10);
+            this.result.Count.Should().Be(10);
+            this.result.ElementAt(0).BomName.Should().Be("ROOT");
+            this.result.ElementAt(1).BomName.Should().Be("n1");
+            this.result.ElementAt(2).BomName.Should().Be("n2");
+            this.result.ElementAt(3).BomName.Should().Be("n3");
+            this.result.ElementAt(4).BomName.Should().Be("n11");
+            this.result.ElementAt(5).BomName.Should().Be("n12");
+            this.result.ElementAt(6).BomName.Should().Be("n13");
+            this.result.ElementAt(7).BomName.Should().Be("n111");
+            this.result.ElementAt(8).BomName.Should().Be("n112");
+            this.result.ElementAt(9).BomName.Should().Be("n1121");
         }
 
         private void BuildBomTree()
@@ -81,7 +95,7 @@
             this.n112
                 = new Bom
                 {
-                    BomName = "ROOT",
+                    BomName = "n112",
                     Details = new List<BomDetail>
                                                           {
                                                               new BomDetail
