1e116f347e1c6069e776c1f4a3e327761f3750f3
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2022-10-28 17:06:47 +0100

    throw exception if story too long

---
+++ src/Domain.LinnApps/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace Linn.Production.Domain.LinnApps
+{
+    using System.Collections.Generic;
+
+    public class Node
+    {
+        public string Name { get; set; }
+
+        public List<Node> Children { get; set; }
+
+        public Node Parent { get; set; }
+    }
+}
---
+++ tests/Unit/Domain.Tests/PartFailServiceSpecs/WhenCreatingAndStoryTooLong.cs
@@ -0,0 +1,52 @@
+﻿namespace Linn.Production.Domain.Tests.PartFailServiceSpecs
+{
+    using System.Collections.Generic;
+
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Domain.LinnApps.Exceptions;
+    using Linn.Production.Domain.LinnApps.Measures;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenCreatingAndStoryTooLong : ContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var story = string.Empty;
+
+            for (int i = 0; i <= 200; i++)
+            {
+                story += "a";
+            }
+
+            this.Candidate = new PartFail
+                                 {
+                                     Id = 1,
+                                     Part = new Part { PartNumber = "PART" },
+                                     PurchaseOrderNumber = 1,
+                                     Story = story
+                                 };
+
+            this.PartRepository.FindById("PART").Returns(new Part { PartNumber = "PART" });
+            this.PurchaseOrderRepository.FindById(1).Returns(new PurchaseOrder
+                                                                 {
+                                                                     Details = new List<PurchaseOrderDetail>
+                                                                                   {
+                                                                                        new PurchaseOrderDetail
+                                                                                            {
+                                                                                                PartNumber = "PART"
+                                                                                            }
+                                                                                   }
+                                                                 });
+        }
+
+        [Test]
+        public void ShouldThrowException()
+        {
+            Assert.Throws<PartFailException>(() => this.Sut.Create(this.Candidate));
+        }
+    }
+}
