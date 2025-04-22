79000c126657c777c6a8d546fd3eac9fa39470c7
Ross Stewart <ross.stewart@linn.co.uk>
2025-04-22 13:58:57 +0100

    missing files

---
+++ src/Facade/Services/IManufacturingResourceFacadeService.cs
@@ -0,0 +1,13 @@
+﻿namespace Linn.Production.Facade.Services
+{
+    using System.Collections.Generic;
+
+    using Linn.Common.Facade;
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Resources;
+
+    public interface IManufacturingResourceFacadeService : IFacadeFilterService<ManufacturingResource, string, ManufacturingResourceResource, ManufacturingResourceResource, ManufacturingResourceResource>
+    {
+        IResult<IEnumerable<ManufacturingResource>> GetValid();
+    }
+}
---
+++ src/Facade/Services/IManufacturingSkillFacadeService.cs
@@ -0,0 +1,13 @@
+﻿namespace Linn.Production.Facade.Services
+{
+    using System.Collections.Generic;
+
+    using Linn.Common.Facade;
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Resources;
+
+    public interface IManufacturingSkillFacadeService : IFacadeFilterService<ManufacturingSkill, string, ManufacturingSkillResource, ManufacturingSkillResource, ManufacturingSkillResource>
+    {
+        IResult<IEnumerable<ManufacturingSkill>> GetValid();
+    }
+}
