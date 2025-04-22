a2c1d93fc4e16a09bf1294007f08ba1ec4e16be1
Ross Stewart <ross.stewart@linn.co.uk>
2025-04-22 13:53:22 +0100

    missing files

---
+++ src/Facade/Services/ManufacturingResourceFacadeService.cs
@@ -0,0 +1,61 @@
+﻿namespace Linn.Production.Facade.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using Linn.Common.Facade;
+    using Linn.Common.Persistence;
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Resources;
+
+    public class ManufacturingResourceFacadeService : FacadeFilterService<ManufacturingResource, string, ManufacturingResourceResource, ManufacturingResourceResource, ManufacturingResourceResource>, IManufacturingResourceFacadeService
+    {
+        private readonly IRepository<ManufacturingResource, string> manufacturingResourceRepository;
+
+        public ManufacturingResourceFacadeService(
+            IRepository<ManufacturingResource, string> manufacturingResourceRepository,
+            ITransactionManager transactionManager)
+            : base(manufacturingResourceRepository, transactionManager)
+        {
+            this.manufacturingResourceRepository = manufacturingResourceRepository;
+        }
+
+        public IResult<IEnumerable<ManufacturingResource>> GetValid()
+        {
+            return new SuccessResult<IEnumerable<ManufacturingResource>>(
+                this.manufacturingResourceRepository.FilterBy(c => !c.DateInvalid.HasValue));
+        }
+
+        protected override ManufacturingResource CreateFromResource(ManufacturingResourceResource resource)
+        {
+            return new ManufacturingResource
+            {
+                ResourceCode = resource.ResourceCode,
+                Description = resource.Description,
+                Cost = resource.Cost,
+                DateInvalid = resource.DateInvalid != null
+                                  ? DateTime.Parse(resource.DateInvalid)
+                                  : (DateTime?)null
+            };
+        }
+
+        protected override void UpdateFromResource(ManufacturingResource manufacturingResource, ManufacturingResourceResource updateResource)
+        {
+            manufacturingResource.Description = updateResource.Description;
+            manufacturingResource.Cost = updateResource.Cost;
+            manufacturingResource.DateInvalid = updateResource.DateInvalid != null
+                                                    ? DateTime.Parse(updateResource.DateInvalid)
+                                                    : (DateTime?)null;
+        }
+
+        protected override Expression<Func<ManufacturingResource, bool>> SearchExpression(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Expression<Func<ManufacturingResource, bool>> FilterExpression(ManufacturingResourceResource searchTerms)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Facade/Services/ManufacturingSkillFacadeService.cs
@@ -0,0 +1,63 @@
+﻿namespace Linn.Production.Facade.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    using Linn.Common.Facade;
+    using Linn.Common.Persistence;
+    using Linn.Production.Domain.LinnApps;
+    using Linn.Production.Resources;
+
+    public class ManufacturingSkillFacadeService : FacadeFilterService<ManufacturingSkill, string, ManufacturingSkillResource, ManufacturingSkillResource, ManufacturingSkillResource>, IManufacturingSkillFacadeService
+    {
+        private readonly IRepository<ManufacturingSkill, string> manufacturingSkillRepository;
+
+        public ManufacturingSkillFacadeService(
+            IRepository<ManufacturingSkill, string> manufacturingSkillRepository, 
+            ITransactionManager transactionManager) 
+            : base(manufacturingSkillRepository, transactionManager)
+        {
+            this.manufacturingSkillRepository = manufacturingSkillRepository;
+        }
+
+        public IResult<IEnumerable<ManufacturingSkill>> GetValid()
+        {
+            return new SuccessResult<IEnumerable<ManufacturingSkill>>(
+                this.manufacturingSkillRepository.FilterBy(c => !c.DateInvalid.HasValue));
+        }
+
+        protected override ManufacturingSkill CreateFromResource(ManufacturingSkillResource resource)
+        {
+            return new ManufacturingSkill
+            {
+                           SkillCode = resource.SkillCode,
+                           Description = resource.Description,
+                           HourlyRate = resource.HourlyRate,
+                           DateInvalid = resource.DateInvalid != null
+                                             ? DateTime.Parse(resource.DateInvalid)
+                                             : (DateTime?)null
+            };
+        }
+
+        protected override void UpdateFromResource(ManufacturingSkill entity, ManufacturingSkillResource updateResource)
+        {
+            entity.SkillCode = updateResource.SkillCode;
+            entity.Description = updateResource.Description;
+            entity.HourlyRate = updateResource.HourlyRate;
+            entity.DateInvalid = updateResource.DateInvalid != null
+                                     ? DateTime.Parse(updateResource.DateInvalid)
+                                     : (DateTime?)null;
+        }
+
+        protected override Expression<Func<ManufacturingSkill, bool>> SearchExpression(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Expression<Func<ManufacturingSkill, bool>> FilterExpression(ManufacturingSkillResource searchTerms)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
