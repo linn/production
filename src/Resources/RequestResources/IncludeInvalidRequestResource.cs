0037e42e058d220718bfde505226c4e9de4e5776
Ross Stewart <ross.stewart@linn.co.uk>
2025-06-09 16:07:07 +0100

    missing file

---
+++ src/Resources/RequestResources/IncludeInvalidRequestResource.cs
@@ -0,0 +1,9 @@
+﻿namespace Linn.Production.Resources.RequestResources
+{
+    using System;
+
+    public class IncludeInvalidRequestResource
+    {
+        public bool? IncludeInvalid { get; set; }
+    }
+}
